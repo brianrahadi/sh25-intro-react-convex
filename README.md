41d81e9bfff355e8cf6d6395c07799434d5f7bf9
Brian Rahadi <brian.rahadi@gmail.com>
2025-10-01 11:49:46 -0700

    convex working

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # sh25-intro-react-convex
 
 1. npm create vite@latest
+
+
+Installing Convex
+https://docs.convex.dev/quickstart/react
+https://docs.convex.dev/functions/mutation-functions
+
+1. Ensure terminal in current root of folder 
+   1. cd sh25-intro-react-convex
+2. npm install convex
+3. npx convex dev
+4. add guestbookData.jsonl
+5. npx convex import --table guestbook guestbookData.jsonl
+6. add convex/schema.ts
+7. add convex/tasks.ts
+8. Connect backend in src/main.tsx
+9. Display data in src/components/GuestbookSection.tsx
--- src/components/GuestbookSection.tsx
+++ src/components/GuestbookSection.tsx
@@ -1,72 +1,78 @@
-// import { useQuery, useMutation } from "convex/react";
-// import { api } from "../../convex/_generated/api";
-// import { useState } from "react";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { useState } from "react";
 
-// export default function GuestbookSection() {
-//     const tasks = useQuery(api.tasks.get);
-//     const addEntry = useMutation(api.myFunctions.createTask);
 
-//     const [name, setName] = useState("");
-//     const [message, setMessage] = useState("");
+export interface GuestbookEntry {
+    name: string;
+    message: string;
+    _creationTime: number;
+}
 
-//     const submitHandler = async (e) => {
-//         e.preventDefault();
+export default function GuestbookSection() {
+    const guestbookEntries = useQuery(api.tasks.get);
+    const addEntry = useMutation(api.tasks.create);
 
-//         // Call Convex API to add a new guestbook entry
-//         await addEntry({ name, message });
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
 
-//         // Clear the form
-//         setName("");
-//         setMessage("");
-//     };
-//     console.log(tasks);
+    const submitHandler = async (e: any) => {
+        e.preventDefault();
 
-//     const formatDate = (timestamp) => new Date(timestamp).toLocaleDateString('en-US', {
-//         year: 'numeric',
-//         month: 'short',
-//         day: 'numeric'
-//     })
+        // Call Convex API to add a new guestbook entry
+        await addEntry({ name, message });
 
-//     return (
-//         <section className="content-container" id="guestbook">
-//             <h2>Guest Book</h2>
-//             <p>Leave a comment or just say hello!</p>
-//             <div className="guestbook--content">
+        // Clear the form
+        setName("");
+        setMessage("");
+    };  
 
-//                 <form className="guestbook--content--form" onSubmit={submitHandler}>
-//                     <p><b>Name</b></p>
-//                     <input
-//                         type="text"
-//                         placeholder="Enter your name"
-//                         value={name}
-//                         onChange={(e) => setName(e.target.value)}
-//                         required
-//                     />
-//                     <p><b>Message</b></p>
-//                     <textarea
-//                         placeholder="Enter your message"
-//                         value={message}
-//                         onChange={(e) => setMessage(e.target.value)}
-//                         required
-//                     ></textarea>
-//                     <button type="submit" className="btn">Submit</button>
-//                 </form>
+    const formatDate = (timestamp: number) => new Date(timestamp).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    })
 
-//                 <div className="guestbook--content--reviews">
-//                     {tasks && tasks.map(task => {
-//                         return (
-//                             <div className="guestbook--content--review">
-//                                 <div className="guestbook--content--review--name">
-//                                     <div className="circle"></div>
-//                                     <b>{task.name}</b>
-//                                 </div>
-//                                 <p className="guestbook--content--review--comment">{task.message}</p>
-//                                 <p className="guestbook--content--review--date">{formatDate(task._creationTime)}</p>
-//                             </div>
-//                         );
-//                     })}
-//                 </div>
-//             </div>
-//         </section>
-//     )
-// }
+    return (
+        <section className="content-container" id="guestbook">
+            <h2>Guest Book</h2>
+            <p>Leave a comment or just say hello!</p>
+            <div className="guestbook--content">
+
+                <form className="guestbook--content--form" onSubmit={submitHandler}>
+                    <p><b>Name</b></p>
+                    <input
+                        type="text"
+                        placeholder="Enter your name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                        required
+                    />
+                    <p><b>Message</b></p>
+                    <textarea
+                        placeholder="Enter your message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                        required
+                    ></textarea>
+                    <button type="submit" className="btn">Submit</button>
+                </form>
+
+                <div className="guestbook--content--reviews">
+                    {guestbookEntries && guestbookEntries.map(entry => {
+                        return (
+                            <div className="guestbook--content--review">
+                                <div className="guestbook--content--review--name">
+                                    <div className="circle"></div>
+                                    <b>{entry.name}</b>
+                                </div>
+                                <p className="guestbook--content--review--comment">{entry.message}</p>
+                                <p className="guestbook--content--review--date">{formatDate(entry._creationTime)}</p>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </section>
+    )
+}
